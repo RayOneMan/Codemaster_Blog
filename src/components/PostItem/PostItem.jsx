1af92d99b85d494a1bee90d59f2c5789e646529a
RayOneMan <89860586+RayOneMan@users.noreply.github.com>
2021-12-10 20:00:12 +0300

    Update PostItem.jsx

--- src/components/PostItem/PostItem.jsx
+++ src/components/PostItem/PostItem.jsx
@@ -1,5 +1,5 @@
 import { useTranslation } from "react-i18next";
-import { Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 import Button from "../UI/Button/Button";
 
@@ -12,18 +12,6 @@ export default function PostItem({ id, body, title, remove, post }) {
 
   return (
     <>
-<<<<<<< HEAD
-=======
-      <Modal
-        visible={isVisibleEdit}
-        setVisible={setIsVisibleEdit}>
-
-        {/*onEditPost и подобные не надоело пробрасывать между компонентами вверх/вниз по дереву?*/}
-        {/*для этого как раз и создан redux/mobx, советую второй, с ним проще*/}
-
-        <PostEdit onEditPost={onEditPost} postBody={post.body} postTitle={post.title}/>
-      </Modal>
->>>>>>> c8f3af9a0a37ca2e47747d0dfa36ea9f2b2e8182
       <div className="post__item">
         <div className="post__content">
           <div className="post__title">{id}. {title} </div>
@@ -32,16 +20,9 @@ export default function PostItem({ id, body, title, remove, post }) {
           </div>
         </div>
         <div className="post__list-btn">
-<<<<<<< HEAD
           <Link
             to={`/posts/${id}`}
             className="post__link-open"
-=======
-          {/*это ссылка, а не кнопка, как и другие места где ты делаешь навигацию*/}
-          <Button
-            type="button"
-            onClick={() => nav(`/posts/${id}`)}
->>>>>>> c8f3af9a0a37ca2e47747d0dfa36ea9f2b2e8182
           >
             {t("OPEN")}
           </Link>
@@ -50,14 +31,6 @@ export default function PostItem({ id, body, title, remove, post }) {
             onClick={() => remove(post)}>
             {t("DELETE")}
           </Button>
-<<<<<<< HEAD
-=======
-          <Button
-            type="button"
-            onClick={() => setIsVisibleEdit(true)}>
-            Изменить
-          </Button>
->>>>>>> c8f3af9a0a37ca2e47747d0dfa36ea9f2b2e8182
         </div>
       </div>
     </>
